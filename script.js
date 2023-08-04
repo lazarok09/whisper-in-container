a58124b533dcfb9edaea9230b280cafe516707ff
lazarok09 <lazarok09@gmail.com>
2023-08-04 10:38:37 -0300

    feat: add form and frontend to manipulate the whisper api

---
+++ script.js
@@ -0,0 +1,68 @@
+const formElement = document.getElementById("audio-form");
+const track = document.getElementById("track");
+const logsTextArea = document.getElementById("logs");
+const submitAudioButton = document.getElementById("submit");
+const BASE_URL = "http://localhost:5000/whisper";
+
+async function sendFileToWhisper(form) {
+  try {
+    const options = {
+      "Content-Type": "multipart/form-data",
+      body: form,
+      method: "POST",
+    };
+
+    submitAudioButton.disabled = true;
+
+    submitAudioButton.innerHTML = "Carregando...";
+
+    const response = await fetch(BASE_URL, options);
+    const data = await response.json();
+    submitAudioButton.innerHTML = "Enviar";
+    submitAudioButton.disabled = false;
+
+    if (response.ok) {
+      logsTextArea.innerHTML = "Sucesso" + JSON.stringify(data, null, 2);
+    } else {
+      logsTextArea.innerHTML = "Erro" + JSON.stringify(data, 2, null);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  form.append("model", "base");
+  
+  sendFileToWhisper(form);
+});
+
+// Function to convert base64-encoded plain text to WebVTT format
+function convertBase64ToWebVTT(base64Text) {
+  // Decode base64 to plain text
+  const plainText = atob(base64Text);
+
+  const lines = plainText.trim().split("\n");
+  const vttLines = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const startTime = (i * 2).toFixed(1); // Time in seconds
+    const endTime = ((i + 1) * 2).toFixed(1); // Time in seconds
+
+    const vttLine = `${startTime} --> ${endTime}\n${lines[i]}\n`;
+    vttLines.push(vttLine);
+  }
+
+  return "WEBVTT\n\n" + vttLines.join("\n");
+}
+const base64PlainText =
+  "V0VCVlRUCgowMDowMC4wMDAgLS0+IDAwOjAyLjAwMApJIGtub3cuCgowMDowNC4wMDAgLS0+IDAwOjA1LjAwMApXaG8gdGhlIGhlbGwgaXMgdGhhdD8KCjAwOjMwLjAwMCAtLT4gMDA6MzIuMDAwClllYWguCgo=";
+
+const webVTTContent = convertBase64ToWebVTT(base64PlainText);
+
+// Create a Blob containing the WebVTT content
+const blob = new Blob([webVTTContent], { type: "text/vtt" });
+
+// Set the src attribute of the track element to the URL of the Blob
+track.src = URL.createObjectURL(blob);
